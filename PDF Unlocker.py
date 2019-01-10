28da3b78adf633ce4f2ac181b619dc6ff16b28c3
JKSTN <44952323+JKSTN@users.noreply.github.com>
2019-01-10 08:55:33 +0000

    Add files via upload

---
+++ PDF Unlocker.py
@@ -0,0 +1,16 @@
+
+import os
+
+
+Wanted_WD = "/Users/Stevenson/Desktop/Jana"
+
+
+os.chdir(Wanted_WD)
+os.getcwd()
+
+
+import pikepdf as pike
+
+
+pdf = pike.open('input.pdf')
+pdf.save('output.pdf')
